6d0a39614826f2ebcdcad6ee5b1e28b7fa607042
Anuj Dubey <anujd2408@gmail.com>
2025-10-16 20:03:49 +0530

    completed

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.get("/file/:filename" , function(req, res){
+  const name = req.params.filename;
+  fs.readFile(name, "utf-8", function(err, data){
+    res.json({
+      data
+    })
+  })
+})
+
+app.listen(3000);
