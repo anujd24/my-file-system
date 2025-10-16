f1038f27a1f68a3c7229cc8529553ba339808472
Anuj Dubey <anujd2408@gmail.com>
2025-10-16 20:38:26 +0530

    updated index.js code

--- index.js
+++ index.js
@@ -5,25 +5,9 @@ const app = express();
 
 app.get("/file/:filename", function(req, res){
   const name = req.params.filename;
-  
-  // Use __dirname to get the absolute path to your files
-  const filePath = path.join(__dirname, name);
-  
-  fs.readFile(filePath, "utf-8", function(err, data){
-    if (err) {
-      console.log("ERROR:", err.message);
-      console.log("Looking for file at:", filePath);
-      return res.json({ 
-        error: true,
-        message: err.message,
-        filename: name,
-        path: filePath
-      });
-    }
-    
+  fs.readFile(name, "utf-8", function(err, data){
     res.json({
-      data: data,
-      success: true
+      data: data
     });
   });
 });
